46e2f37d1fbe3d491847f63324e2d71ced1431ac
Yik Wai Ng Jason <ywng@MacBook-Pro-4.fios-router.home>
2017-11-14 01:58:15 -0500

    init setup

--- fb_chatbot/views.py
+++ fb_chatbot/views.py
@@ -15,7 +15,7 @@ def post_facebook_message(fbid, recevied_message):
     user_details_url = "https://graph.facebook.com/v2.6/%s"%fbid 
     user_details_params = {'fields':'first_name,last_name,profile_pic', 'access_token':PAGE_ACCESS_TOKEN} 
     user_details = requests.get(user_details_url, user_details_params).json() 
-    echo_text = "Yo "+user_details['first_name']+ " " + recevied_message
+    echo_text = "Yo ~ " + recevied_message
                    
     post_message_url = 'https://graph.facebook.com/v2.6/me/messages?access_token=%s'%PAGE_ACCESS_TOKEN
     response_msg = json.dumps({"recipient":{"id":fbid}, "message":{"text":echo_text}})
